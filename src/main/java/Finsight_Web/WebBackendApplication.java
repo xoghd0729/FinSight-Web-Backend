7217ea74b261f1024c728681cad076bfc22613ef
xoghd0729 <132333838+xoghd0729@users.noreply.github.com>
2026-03-31 17:48:25 +0900

    feat: Spring Boot 초기 환경 설정 및 MySQL DB 연동 완료 (application.yml)

---
+++ src/main/java/Finsight_Web/WebBackendApplication.java
@@ -0,0 +1,13 @@
+package Finsight_Web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Finsight_Web/WebBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package Finsight_Web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
